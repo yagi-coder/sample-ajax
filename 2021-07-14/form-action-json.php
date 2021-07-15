2c4746e45ad956835be675c3ea803bdd14de3962
yagi-coder <87103190+yagi-coder@users.noreply.github.com>
2021-07-15 11:27:23 +0900

    Add files via upload

---
+++ 2021-07-14/form-action-json.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting( E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED );
+session_cache_limiter('nocache');
+session_start();
+header( "Access-Control-Allow-Origin: *" );
+header( "Content-Type: application/json; charset=utf-8" );
+
+// ***********************************
+// JSON データを作成
+// ***********************************
+$json = new stdClass;
+
+$json->get = $_GET;
+$json->post = $_POST;
+$json->session = $_SESSION;
+$json->path = $_SERVER["PHP_SELF"];
+
+print json_encode( $json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT );
+
+?>
